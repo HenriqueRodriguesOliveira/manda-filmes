635bd18b7a7745f92398bdbc4913c00a67228d9e
Henrique Rodrigues <henrique_krf@outlook.com>
2024-01-16 09:09:43 -0300

    Requisições HTTP

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import api from './src/services/api';
+import Filmes from './src/Filmes';
+
+interface FilmesProps {
+  nome: string,
+  id: string
+}
 
 export default function App() {
+
+  const [filmes, setFilmes] = useState<FilmesProps[]>([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data)
+    }
+
+    loadFilmes()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <Filmes data={item} />}
+      />
     </View>
   );
 }
